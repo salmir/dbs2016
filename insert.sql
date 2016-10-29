534577c4c291e2ef22fdfce3c7d54af0ba1e0a1f
Salmir (Desktop) <salmir.delalic@gmail.com>
2016-10-29 23:18:16 +0200

    paar inserts

---
+++ insert.sql
@@ -0,0 +1,12 @@
+begin;
+INSERT INTO Person VALUES
+(1,'vname1','nname1','2001-11-11','adresse1'),
+(2,'vname2','nname2','2001-11-12','adresse2'),
+(3,'vname3','nname3','2001-11-13','adresse3'),
+(4,'vnkunde1','nnkunde1','2001-11-03','adresse11'),
+(5,'vnkunde2','nnkunde2','2001-11-02','adresse12'),
+(6,'vnkunde3','nnkunde3','2001-11-01','adresse13'),
+(7,'vma1','nma1','2001-10-01','adresse01'),
+(8,'vma2','nma2','2001-10-02','adresse02'),
+(9,'vma3','nma3','2001-10-03','adresse03');
+commit;
